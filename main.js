8c8304dd3643250ed8590808d62cd103132d152a
almamun528 <ayapths@gmail.com>
2024-09-23 16:34:32 +0600

    Feni and Nuakhali Function is end

--- main.js
+++ main.js
@@ -1,1 +1,93 @@
-console.log("main js file added")
+//! button switch blog button to home button
+
+document.getElementById("btn-blog").addEventListener("click", function () {
+  window.location.href = "/blog.html";
+});
+document.getElementById("btn-home").addEventListener("click", function () {
+  window.location.href = "/index.html";
+});
+
+//! History Button and Donation button Toggle
+
+document.getElementById("btn-history").addEventListener("click", function () {
+  document.getElementById("history-data").classList.remove("hidden");
+  document.getElementById("card-section").classList.add("hidden");
+});
+
+document.getElementById("btn-donate").addEventListener("click", function () {
+  document.getElementById("history-data").classList.add("hidden");
+  document.getElementById("card-section").classList.remove("hidden");
+});
+
+// !Donate for Flood at Noakhali, Bangladesh Item 1
+document
+  .getElementById("donate-now-btn-nuakhali")
+  .addEventListener("click", function () {
+    const addMoney = getValueByIdFromInput("donate-For-nuakhali-input");
+
+    const donarBalance = donationValueAmount("main-balance");
+
+    if (addMoney > donarBalance) {
+      alert("you don't have sufficient balance for donating");
+      return;
+    }
+    if (isNaN(addMoney)) {
+      alert("Wrong input this only input accepts only Number");
+      return;
+    }
+
+    const getMoney = donationValueAmount("donation-balance-Nuakhali-has");
+
+    const donaterBalanceISNow = donarBalance - addMoney;
+
+    document.getElementById("main-balance").innerText = donaterBalanceISNow;
+
+    const totalDonationGetNuakhali = addMoney + getMoney;
+    document.getElementById("donation-balance-Nuakhali-has").innerText =
+      totalDonationGetNuakhali;
+
+    // Transaction section created
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+            <p class ='p-3'> You donate ${addMoney} tk for <span class ="text-green-500 font-semibold"> Nuakhaki</span>, Transaction date is ${new Date().toLocaleDateString()}</p>
+        `;
+    document.getElementById("transaction-container").appendChild(div);
+  });
+
+//!Donate for Flood Relief in Feni,Bangladesh
+
+document
+  .getElementById("donate-btn-for-feni")
+  .addEventListener("click", function () {
+    const inputMoney = getValueByIdFromInput("donate-feni-input");
+    const donarBalance = donationValueAmount("main-balance");
+
+    if (inputMoney > donarBalance) {
+      alert("you don't have sufficient balance for donating");
+      return;
+    }
+    if (isNaN(inputMoney)) {
+      alert("Wrong input this only input accepts only Number");
+      return;
+    }
+
+    //function calculation
+
+    const feniHasBalance = donationValueAmount("donation-balance-feni");
+
+    const donaterBalanceISNow = donarBalance - inputMoney;
+
+    document.getElementById("main-balance").innerText = donaterBalanceISNow;
+
+    const totalDonationGetFeni = inputMoney + feniHasBalance;
+    document.getElementById("donation-balance-feni").innerText = totalDonationGetFeni
+
+        // Transaction section created
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+                <p class ='p-3'> You donate ${inputMoney} tk for <span class ="text-blue-500 font-semibold">Feni</span>, Transaction date is ${new Date().toLocaleDateString()}</p>
+            `;
+        document.getElementById("transaction-container").appendChild(div);
+  });
--- root.js
+++ root.js
@@ -1,1 +1,12 @@
-console.log('root file')
+// ! get value from User 
+
+function getValueByIdFromInput(id){
+    const getValueByInput = parseFloat(document.getElementById(id).value);
+    return getValueByInput;
+}
+
+// ! get value from text 
+function donationValueAmount(id){
+    const getValueByText = parseFloat(document.getElementById(id).innerText);
+    return getValueByText;
+}
